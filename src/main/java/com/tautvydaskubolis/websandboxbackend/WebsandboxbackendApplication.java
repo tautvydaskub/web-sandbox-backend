f91a63ae6a25561d1822a17f6020c67e8c55c4e5
tautvydaskub <kubolis.tautvydas@gmail.com>
2021-03-06 17:25:56 +0200

    init

---
+++ src/main/java/com/tautvydaskubolis/websandboxbackend/WebsandboxbackendApplication.java
@@ -0,0 +1,13 @@
+package com.tautvydaskubolis.websandboxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsandboxbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsandboxbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tautvydaskubolis/websandboxbackend/WebsandboxbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tautvydaskubolis.websandboxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsandboxbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
